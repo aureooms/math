b8b34b1fa27572929c62a57e4c48b5caf5e59a85
Aurélien Ooms <aurelien.ooms@gmail.com>
2019-09-24 17:57:56 +0200

    :recycle: refactor(theme): Remove source.

--- themes/events/assets/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var dropdowns = document.querySelectorAll('.dropdown-trigger');
-    M.Dropdown.init(dropdowns);
-    var sidenavs = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenavs);
-});
